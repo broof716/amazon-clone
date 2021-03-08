ac2200e308e18744c5dd3a19bd9a77e7cbc332be
Bryan Roof <broof716@gmail.com>
2021-03-08 16:03:21 -0700

    Setting up Header.css, and Header.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>Amazon Store Application</h1>
+      <Header />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,45 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+
 
 function Header() {
   return (
-    <div className='header'>
-      
+    <div className="header">
+      <img className="header__logo" 
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+      />
+
+      <div className="header__search">
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header_searchIcon" />
+      </div>
+      <div className="header__nav">
+        <div className='header__option'>
+          <span className='header__opitionLineOne'>
+            Hello Guest
+          </span>
+          <span className='header__opitionLineTwo'>
+            Sign In
+          </span>
+        </div>
+        <div className='header__option'>
+          <span className='header__opitionLineOne'>
+            Returns
+          </span>
+          <span className='header__opitionLineTwo'>
+            & Orders
+          </span>
+        </div>
+        <div className='header__option'>
+        <span className='header__opitionLineOne'>
+            Your
+          </span>
+          <span className='header__opitionLineTwo'>
+            Prime
+          </span>
+        </div>
+      </div>
     </div>
   )
 }
