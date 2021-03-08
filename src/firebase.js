7da3b7bf8087a6b7262e12ff550e2a5343a84d8e
Bryan Roof <broof716@gmail.com>
2021-03-07 17:12:57 -0700

    Setting up Header.css, and Header.js file

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+const firebaseConfig = {
+  apiKey: "AIzaSyB7dyiZmj7lMOeeO97yutQgvKQfSrdUYKg",
+  authDomain: "clone-596ae.firebaseapp.com",
+  projectId: "clone-596ae",
+  storageBucket: "clone-596ae.appspot.com",
+  messagingSenderId: "150357231932",
+  appId: "1:150357231932:web:00b906b90fc4e6e6303639",
+  measurementId: "G-Q6276D1904"
+};
