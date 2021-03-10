9f13cb7d8b6412879223b6c823166b935821ee72
Bryan Roof <broof716@gmail.com>
2021-03-10 16:59:57 -0700

    creating sections for checkout page

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from './Checkout';
 
 
 function App() {
   return (
     <Router>
       <div className="app">
+        <Header />
+
         <Switch>
-        <Route path="/checkout">
-            <Header />
-            <h1>I AM A CHECKOUT, SMASH THE LIKE BUTTON</h1>
-          </Route>
+          <Route path="/checkout">
+            <Checkout />
+              </Route>
           <Route path="/">
-            <Header />
             <Home />
           </Route>
         </Switch>
