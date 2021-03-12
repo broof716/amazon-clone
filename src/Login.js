5e946d953dd6850dde1eb26337d0f2aeac0621e9
Bryan Roof <broof716@gmail.com>
2021-03-12 16:38:32 -0700

    building user authentication

--- src/Login.js
+++ src/Login.js
@@ -6,6 +6,16 @@ function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const signIn = e => {
+    e.preventDefault();
+
+  }
+
+  const register = e => {
+    e.preventDefault();
+
+  }
+
   return (
     <div className="login">
       <Link to='/'>
@@ -23,7 +33,7 @@ function Login() {
           <h5>Password</h5>
           <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
           
-          <button className="login__signInButton">Sign In</button>
+          <button type="submit" onClick={signIn} className="login__signInButton">Sign In</button>
         </form>
 
         <p>
@@ -31,7 +41,7 @@ function Login() {
           see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
         </p>
 
-        <button className="login__registerButton">Create Your Amazon Account</button>
+        <button onClick={register} className="login__registerButton">Create Your Amazon Account</button>
       </div>
     </div>
   )
