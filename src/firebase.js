c42a2cf9e6f29a8d59f5ac596725ba73d291913c
Bryan Roof <broof716@gmail.com>
2021-03-12 16:44:05 -0700

    building user authentication using firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
-// eslint-disable-next-line no-unused-vars
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyB7dyiZmj7lMOeeO97yutQgvKQfSrdUYKg",
   authDomain: "clone-596ae.firebaseapp.com",
@@ -8,3 +9,10 @@ const firebaseConfig = {
   appId: "1:150357231932:web:00b906b90fc4e6e6303639",
   measurementId: "G-Q6276D1904"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
