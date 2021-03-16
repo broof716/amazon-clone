9a13d820b4bef08669406d0424a7eac857ec6c28
Bryan Roof <broof716@gmail.com>
2021-03-15 18:53:40 -0700

    installed strip.api

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ import Login from "./Login";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
 import  Payment  from './Payment';
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
+const promise = loadStripe('pk_test_YnFHIK1q9mPhPpi6KcM87PLU00AF1qRQqE');
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -49,7 +52,9 @@ function App() {
           </Route>
           <Route path="/payment">
             <Header />
-            <Payment />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           <Route path="/">
             <Header />
--- src/Payment.js
+++ src/Payment.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import React, { useEffect, useState } from 'react';
+import CurrencyFormat from 'react-currency-format';
 import { Link } from 'react-router-dom';
 import CheckoutProduct from './CheckoutProduct';
 import './Payment.css'
+import { getBasketTotal } from './reducer';
 import { useStateValue } from './StateProvider';
 
 function Payment() {
   const [{ basket, user }, dispatch] = useStateValue();
+
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const [succeeded, setSucceeded] = useState(false);
+  const [processing, setProcessing] = useState("");
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(true);
+  const [clientSecret, setClientSecret] = useState(true);
+
+  useEffect(() => {
+    // generate the special stripe secret which allows us to change a customer
+
+    const getClientSecret = async () => {
+      
+    }
+    getClientSecret();
+  }, [basket])
+
+  const handleSubmit = async(event) => {
+    event.preventDefault();
+    setProcessing(true);
+
+    // const payload = await stripe
+  }
+
+  const handleChange = event => {
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : "");
+  }
 
   return (
     <div className="payment">
@@ -44,7 +77,26 @@ function Payment() {
             <h3>Payment Method</h3>
           </div>
           <div className="payment__details">
+            <form onSubmit={handleSubmit}>
+              <CardElement  onChange={handleChange}/>
 
+              <div className="payment__priceContainer">
+              <CurrencyFormat 
+                renderText={(value) => (
+                    <h3>Order Total: {value}</h3>
+                )}
+                decimalScale={2}
+                value={getBasketTotal(basket)}
+                displayType={"text"}
+                thousandSeparator={true}
+                prefix={"$"}
+                />
+                <button disabled={processing || disabled || succeeded}>
+                  <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
+                </button>
+              </div>
+              {error && <div>{error}</div>}
+            </form>
           </div>
         </div>
       </div>
