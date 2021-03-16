7b01c94b50fdb056c94d5fccbf90d0133c3d6775
Bryan Roof <broof716@gmail.com>
2021-03-16 15:27:14 -0700

    setup cloud firestore

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { useStateValue } from "./StateProvider";
 import  Payment  from './Payment';
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
+import Orders from "./Orders";
 
 const promise = loadStripe('pk_test_YnFHIK1q9mPhPpi6KcM87PLU00AF1qRQqE');
 
@@ -43,6 +44,10 @@ function App() {
     <Router>
       <div className="app">
         <Switch>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route>
           <Route path="/login">
             <Login />
           </Route>
