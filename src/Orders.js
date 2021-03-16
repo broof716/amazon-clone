74129fcbf38bb953b0cf3ff213ec8df886a678bf
Bryan Roof <broof716@gmail.com>
2021-03-16 16:12:55 -0700

    setup Order.css

--- src/Orders.js
+++ src/Orders.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { db } from './firebase';
+import Order from './Order';
 import './Orders.css';
 import { useStateValue } from './StateProvider';
 
@@ -8,22 +9,31 @@ function Orders() {
   const [orders, setOrders] = useState([]);
 
   useEffect(() => {
-    db
-      .collection('users')
-      .doc(user?.id)
-      .collection('orders')
-      .orderBy('created', 'desc')
-      .onSnapshot(snapshot => (
-        setOrders(snapshot.docs.map(doc => ({
-          id: doc.id,
-          data: doc.data()
+    if(user) {
+      db
+        .collection('users')
+        .doc(user?.uid)
+        .collection('orders')
+        .orderBy('created', 'desc')
+        .onSnapshot(snapshot => (
+          setOrders(snapshot.docs.map(doc => ({
+            id: doc.id,
+            data: doc.data()
         })))
       ))
-  }, [])
+    } else {
+      setOrders([])
+    }
+  }, [user])
 
   return (
     <div className="orders">
       <h1>Your Orders</h1>
+      <div className="orders__order">
+        {orders?.map(order => (
+          <Order order={order} />
+        ))}
+      </div>
     </div>
   )
 }
--- src/Payment.js
+++ src/Payment.js
@@ -50,7 +50,7 @@ function Payment() {
 
       db
       .collection('users')
-      .doc(user?.id)
+      .doc(user?.uid)
       .collection('orders')
       .doc(paymentIntent.id)
       .set({
